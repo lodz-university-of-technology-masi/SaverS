ee5507fc9abb3d1fe6aa7adf9bb7dfd1a7a0ed0a
KonradK98 <zangetsuxxl2@gmail.com>
2019-12-15 16:27:35 +0100

    js fix

--- Front/testModule/test.js
+++ Front/testModule/test.js
@@ -23,122 +23,16 @@ function getJsonData() {
     })
   })
 };
+$(document).ready(function () {
 
-$(document).ready( function() {
+  getJsonData().then(
+    () => {
+      //$('#alert_placeholder').html('<div class="alert"><a class="close" data-dismiss="alert">×</a><span>'+test.questions[1].content+'</span></div>')
 
- getJsonData().then(
-   () => {
-  //$('#alert_placeholder').html('<div class="alert"><a class="close" data-dismiss="alert">×</a><span>'+test.questions[1].content+'</span></div>')
+      var testDIV = document.getElementById("placeholder");
+      const app = $("#test");
 
-  var testDIV = document.getElementById("placeholder");
-  const app = $("#test");
 
-  for(let i in test.questions){
-
-    //creating alert with question
-    var questionDiv = document.createElement("q1");
-    questionDiv.classList.add("alert", "alert-warning");
-    questionDiv.classList.add("role", "alert");
-
-    //creating test node with question
-    var questionText = document.createTextNode(test.questions[i].content); 
-    questionDiv.appendChild(questionText);
-
-    //separator
-    var p = document.createElement("p");
-
-    testDIV.appendChild(questionDiv);
-    testDIV.appendChild(p);
-
-  
-    if (test.questions[i].answers == "|"){
-      
-      var answerDiv = document.createElement("div");
-      answerDiv.classList.add("div", "input-group-prepend");
-      
-      var answSpan = document.createElement("span");
-      answSpan.classList.add("span", "input-group-text");
-      var answText = document.createTextNode("Answer");
-      answSpan.appendChild(answText);
-      answerDiv.appendChild(answSpan);
-      
-      var answ = document.createElement("input");
-      answ.classList.add("form", "form-control");
-      answ.id = i;
-      answerDiv.appendChild(answ);
-  
-      var b = document.createElement("br");
-      testDIV.appendChild(b);
-      testDIV.appendChild(answerDiv);
-      testDIV.appendChild(b);
-    }
-    else {
-  
-      for (let j in test.questions[i].answers){
-        var checkBoxDiv = document.createElement("div");
-        checkBoxDiv.classList.add("div", "form-check");
-
-        var checkboxInput = document.createElement("input");
-        checkboxInput.classList.add("input", "form-check-input");
-        checkboxInput.type = "checkbox";
-        checkboxInput.id="check"+j;
-        var label = document.createElement("answ");
-        label.classList.add("label", "form-check-label");
-        var labelText = document.createTextNode(test.questions[i].answers[j]);
-        label.appendChild(labelText);
-        
-        checkBoxDiv.appendChild(checkboxInput);
-        checkBoxDiv.appendChild(label);
-
-        testDIV.appendChild(checkBoxDiv);
-        
-        // app.append("<input id=check"  + j + " type=checkBox>" + test.questions[i].answers[j] + "</input>");
-      }
-      var br = document.createElement("br");
-      testDIV.appendChild(br);
-  }
-}
-testDIV.appendChild(QA);
-
-  /*
-    for (let i in test.questions) {
-        //app.append("<p>" + test.questions[i].content + "</p>");
-      if (test.questions[i].answers == "|")
-        app.append("<input id=" + i + ">" + "</input>");
-      else
-        // console.log(ans)
-        for (let j in test.questions[i].answers)
-          app.append("<input id=check"  + j + " type=checkBox>" + test.questions[i].answers[j] + "</input>");
-    }
-    */
-
-    // const ans = $('#answers')
-    // zapiszTest = () => {
-    // //   x = document.getElementById("1").value;
-    
-    //   for (let i in test.questions) {
-    //     if (test.questions[i].answers == "|"){
-    //       console.log(document.getElementById(i).value)
-    //     ans.append(document.getElementById(i).value+"<br>")
-    //     }
-    //     else
-    //       // console.log(ans)
-    //       for (let j in test.questions[i].answers){
-    //       console.log(document.getElementById("check"+j).checked)
-    //     ans.append(document.getElementById("check"+j).checked+"<br>")  
-    //     }
-    //         // app.append("<input id=" + i + " type=checkBox>" + ans + "</input>");
-    //   }
-    // };
-   }
- );
-
-});
-// let zapiszTest;
-const ans = $('#answers')
-    zapiszTest = () => {
-    //   x = document.getElementById("1").value;
-    
       for (let i in test.questions) {
 
         //creating alert with question
@@ -228,6 +122,3 @@ let zapiszTest = function () {
     // app.append("<input id=" + i + " type=checkBox>" + ans + "</input>");
   }
 }
-
-
-
