cd1f8a9613e93a979354c74aae53f6770e8bbb54
Jenczu11 <bartek.jencz@wp.pl>
2020-01-02 15:09:48 +0100

    added score to red button

--- Front/js/mainCandidateScrpits.js
+++ Front/js/mainCandidateScrpits.js
@@ -3,11 +3,27 @@ let testID;
 let assigns;
 let tests = [];
 let test;
-
 getAssigns();
 assigns = assigns.attributions;
 createTestButtons();
 
+function getEvaluatedTest(id) {
+  return new Promise((resolve, reject) => {
+    $.ajax({
+      url: `https://dj9pgircgf.execute-api.us-east-1.amazonaws.com/SaversAPI/answer/${id}`,
+      type: "GET",
+      success: data => {
+        
+        console.log(JSON.parse(data.body));
+        return resolve(data);
+      },
+      error: err => {
+        console.log(err.responseJSON);
+        return reject(err.responseText)
+      }
+    })
+  })
+};
 function getAssigns() {
     $.ajax({
       url: `https://dj9pgircgf.execute-api.us-east-1.amazonaws.com/SaversAPI/attribution/candidate/${myParam}`,
@@ -32,6 +48,7 @@ function getTest() {
       success: data => {
         console.log(data);
         test = JSON.parse(data.body);
+       
       },
       error: err => {
         console.log(err.responseJSON);
@@ -61,6 +78,8 @@ function createTestButtons() {
         let newButton = document.createElement("button");
         newButton.type = "button";
         newButton.classList.add("button", "btn");
+        let newButtonText = document.createTextNode(tests[currentTest].test.name);
+        newButton.appendChild(newButtonText);
         if(tests[currentTest].state==0){
           // Przypisany
           newButton.classList.add("button", "btn-outline-success");
@@ -72,6 +91,19 @@ function createTestButtons() {
         if(tests[currentTest].state==2){
           // Oceniony
           newButton.classList.add("button", "btn-outline-danger");
+          // Dopisz wynik do czerwonego przycisku <- generuje get na każdy rozwiazany test
+          evalID = getUserName()+ "-"+ tests[currentTest].test.id
+          getEvaluatedTest(evalID).then((score) => {
+           let evaluatedTest = JSON.parse(score.body);
+           let points = evaluatedTest.points;
+           let totalPoints = evaluatedTest.test.questions.length;
+           scoreText = `Your score is ${points} out of ${totalPoints}`;
+           console.log(scoreText)
+           var br = document.createElement("br");
+           var content = document.createTextNode(scoreText);
+           newButton.appendChild(br);
+           newButton.appendChild(content);
+          })
         }
         
         newButton.classList.add("button", "btn-lg");
@@ -81,8 +113,7 @@ function createTestButtons() {
                 window.open("testModule/test.html?id=" + tests[currentTest].test.id, "_self");
             }
         );
-        let newButtonText = document.createTextNode(tests[currentTest].test.name);
-        newButton.appendChild(newButtonText);
+        
         tableDiv.appendChild(newButton);
     }
 }
