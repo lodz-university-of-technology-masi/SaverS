eb8f6ebe82ddca409db40701ef78548aa0ca9891
Jan Karwowski <karwojan@tlen.pl>
2020-01-16 18:49:43 +0100

    Small fix in minimalsafety

--- Front/js/minimalsafety.js
+++ Front/js/minimalsafety.js
@@ -4,14 +4,15 @@ function checkIfCookieEmpty(){
         window.open(`${window.location.origin}/index.html`,"_self")
     }
 }
-function checkForCandidate() {
 
+function checkForCandidate() {
     checkIfCookieEmpty();
-    if(JSON.parse(document.cookie).usertype != 1){
+    if(JSON.parse(document.cookie).usertype != 2){
         alert("Nie jestes upowazniony do tego zasobu");
         window.open(`${window.location.origin}/index.html`,"_self")
     }
 }
+
 function checkForRecruiter(){
     checkIfCookieEmpty();
     if(JSON.parse(document.cookie).usertype != 1){
